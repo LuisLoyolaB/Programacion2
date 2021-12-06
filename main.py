665e3e7ddcbac7bdfd046fc161923b9a6523bad4
Luis Loyola <luis.loyola10@inacapmail.cl>
2021-12-05 22:24:31 -0300

    Creacion
    aqui tenemos seteada la base de datos con pyton

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-sdf
+import mysql.connector,os
+from mysql.connector.errors import Error
+os.system("cls")
+
+try:
+    conectar = mysql.connector.connect(
+        host = 'localhost',
+        user = 'root',
+        password = '',
+        database ='boliche'
+    )
+    if conectar.is_connected():
+        print("Conexión Exitosa!!!")
+        cursor = conectar.cursor()
+        cod = int(input("Ingresa código : "))
+        det = input("Ingresa Detalle: ")
+        pre= int(input("Ingresa Precio: "))
+        sql = "INSERT INTO producto(codigo,detalle,precio) values (%s,%s,%s)"
+        cursor.execute(sql,(cod,det,pre))
+        conectar.commit() # Confirmar la acción
+        print("\n Registro Ingresado \n ")
+except Error as e:
+    print(f"error en la conexión : {e}")
+
+finally:
+    if conectar.is_connected():
+        conectar.close()
+        print("Conexión Finalizada")
